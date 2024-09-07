495fd06ad44d106dfe8e459b6385f97c7cd1ff91
Wilson <wtovaragudelo@gmail.com>
2024-09-08 00:31:25 +0200

    fix episode pub date

--- start/view.ts
+++ start/view.ts
@@ -5,10 +5,14 @@ import { marked } from 'marked'
 // import env from '#start/env'
 
 edge.global('markdown', (value: string) => marked.parse(value))
-edge.global('humanDate', (value: number) =>
-  DateTime.fromSeconds(value).toLocaleString({
+edge.global('humanDate', (value: number | DateTime) => {
+  if (typeof value === 'number') {
+    value = DateTime.fromSeconds(value)
+  }
+
+  return value.toLocaleString({
     month: 'short',
     year: 'numeric',
     day: '2-digit',
   })
-)
+})
